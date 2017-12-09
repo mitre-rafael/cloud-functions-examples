e7765a7871f08e093919d2b1af8319b2b43e09da
rafael mitre <rafael.mitre@gmail.com>
2017-12-09 17:09:42 +0100

    filter example added

--- chatFilter/functions/index.js
+++ chatFilter/functions/index.js
@@ -1,8 +1,34 @@
 const functions = require('firebase-functions');
 
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.chatFilter = functions.database.ref('posts/{pushId}')
+.onWrite(event => {
+
+  const post = event.data.val();
+
+  if (post.filtered){
+    return
+  }
+  console.log("filtering...")
+  post.filtered = true;
+  console.log(post.title)
+  post.title = filter(post.title);
+  post.body = filter(post.body);
+
+  return event.data.ref.set(post);
+
+})
+function filter (string){
+  var filteredText = string;
+
+  filteredText = filteredText.replace(/\bstupid\b/ig, "wonderful");
+  return filteredText;
+
+}
